a7622c9f915d1d991552840d8797fcdba88e65c1
adilkadivala <adilkadivala560@gmail.com>
2024-11-13 09:44:57 +0530

    clerk error

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
- if (!isPublicRoute(request)) {
-  await auth.protect()
- }
-})
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
+
 
 export const config = {
- matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
